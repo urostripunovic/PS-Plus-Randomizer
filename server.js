faf1081419e88403025bb72d899e2f0954d886c6
urostripunovic <uros_1997@hotmail.com>
2023-07-30 18:31:09 +0200

    Express server done

---
+++ server.js
@@ -0,0 +1,85 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import puppeteer from 'puppeteer';
+import rateLimit from 'express-rate-limit'
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.port || 9000;
+dotenv.config();
+
+const limiterScraper = rateLimit({
+  windowMs: 15 * 60 * 1000, // 59 minutes
+  max: 1, // Limit each IP to 1 requests per `window` scraping the website
+})
+
+const limiterPlatPrices = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes paus
+  max: 500, // Limit each IP to 1 requests per `window` scraping the website
+})
+//app.use('/api/psplusgames', limiterScraper);
+//app.use('/api/platprices', limiterPlatPrices);
+app.set("trust proxy", 1);
+
+app.use(cors());
+
+const SCRAPING_URL = process.env.VITE_SCRAPING_WEBSITE;
+const PLAT_PRICES_URL = process.env.VITE_PLAT_PRICES_URL;
+const PLAT_PRICES_API_KEY = process.env.VITE_PLAT_PRICES_API_KEY;
+
+//cache this respone for the next two weeks
+app.get('/api/psplusgames', async (req, res) => {
+  const psplusgame = await run();
+  res.send(psplusgame);
+})
+
+//cache the value for the next day
+app.get('/api/platprices', async (req, res) => {
+  try {
+    const response = await axios.get(`${PLAT_PRICES_URL}`, {
+      params: {
+        key: PLAT_PRICES_API_KEY,
+        name: req.query.name,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+const run = async () => {
+  let browser;
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+    await page.goto(`${SCRAPING_URL}`);
+
+    const extractedData = await page.evaluate(() => {
+      const aTags = document.querySelectorAll('div.games-filtered ul.games-style-list li a');
+      const data = [];
+
+      aTags.forEach((aTag) => {
+        data.push(aTag.textContent.trim());
+      });
+
+      return [...new Set(data)];
+    });
+
+    await browser.close();
+
+    return extractedData;
+  } catch (error) {
+    console.log("scraping failed", error);
+  } finally {
+    browser?.close();
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [data, setData] = useState(null);
+  const [nbr, setNbr] = useState(null);
+
+  /*useEffect(() => {
+    fetch("http://localhost:9000/api/psplusgames")
+    .then((res) => res.json())
+    .then(data => console.log(data));
+  }, [])*/
+
+  const testa = () => {
+    fetch("http://localhost:9000/api/psplusgames")
+      .then((res) => res.json())
+      .then(response => {
+        setData(response);
+        console.log(data)
+      })
+  }
+
+  const testa2 = () => {
+    const randomNbr = getRandomNumber(0, data.length);
+    fetch(`http://localhost:9000/api/platprices?name=${data[randomNbr]}`)
+      .then(res => res.json())
+      .then(ans => {
+        console.log(ans)
+      })
+  }
+
+  const testa3 = () => {
+    console.log(data)
+  }
+
+  const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      App
+      <button onClick={() => testa()}>fetch scraper</button>
+      <button onClick={() => testa2()}>get platprices value</button>
+      <button onClick={() => testa3()}>get fetched value</button>
+    </div>
   )
 }
 
-export default App
